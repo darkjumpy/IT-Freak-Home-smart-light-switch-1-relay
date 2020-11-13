56aa8b7917af83884ebe06640ed4799b53da0687
Jakub Kołodziejczyk <58175895+IT-freak-Jake@users.noreply.github.com>
2020-11-13 13:26:56 +0100

    Change from mesh network to direct MQTT connection

--- light-switch.ino
+++ light-switch.ino
@@ -1,206 +1,173 @@
-#include "painlessMesh.h"
+#include <ESP8266WiFi.h>    
+#include <PubSubClient.h>   
+#include <ESP8266mDNS.h>    
+#include <WiFiUdp.h>        
+#include <ArduinoOTA.h>     
 #include <EEPROM.h>
 
-#define   MESH_PREFIX     "YourMeshNetworkSSID" 
-#define   MESH_PASSWORD   "YourMeshNetworkPassword"
-#define   MESH_PORT       5555
-//every node have to have this same SSID, password and port
+// WiFi configuration
+#define wifi_ssid "Your WiFi name"
+#define wifi_password "Your WiFi password"
 
+// MQTT configuration
+#define mqtt_server "MQTT broker IP (example: 192.168.1.11)"
+#define mqtt_user "MQTT username"
+#define mqtt_password "MQTT password"
+
+// Pins configuration
 #define RELAY 14 //It's pin D5 for wemos D1 mini
 #define SWITCH 13 //It's pin D7 for wemos D1 mini
 
-painlessMesh  mesh;
+void mqttCallback(char* topic, byte* payload, unsigned int length);
 
-int addr = 0;
-bool isLightOn=false, isGrootConnected=false, prevSwitchState, isConnectionTesting=false, isTestPassed=true;
-unsigned long prevMillisConnectionTest=0, intervalConnectionTest=300000, prevMillisConnectionTestWait=0, intervalConnectionTestWait=2000; 
-String nodeId, msg, topic, RootAskingMessage="whoIsGroot", RootReciveMessage="imGroot";
-uint32_t GrootId;
+WiFiClient espClient;
+PubSubClient mqtt_client(mqtt_server, 1883, mqttCallback, espClient);
 
-void sendMessage();
+//WiFiServer TelnetServer(8266);
 
-void receivedCallback( uint32_t from, String &msg ) 
+void setup_wifi() 
 {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
-  if(isGrootConnected==true)
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.print(wifi_ssid);
+  WiFi.begin(wifi_ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED) 
   {
-    if(msg=="ON")
-    {
-      isLightOn=true;
-      sendMessageAndTopic("ON", topic);
-      digitalWrite(RELAY, HIGH);
-    }
-    else if(msg=="OFF")
-    {
-      isLightOn=false;
-      sendMessageAndTopic("OFF", topic);
-      digitalWrite(RELAY, LOW);
-    }
+    delay(500);
+    Serial.print(".");
   }
-  if(msg=="imGroot")
-  {
-    GrootId=from;
-    Serial.print("Groot ID: ");
-    Serial.println(GrootId);
-    sendStartupMessage();
-  }
-  else if(isGrootConnected==false)
-  {
-    askAboutGroot();
-  }
-  if(msg=="pong")
-  {
-    isTestPassed=true;
-  }
+  Serial.println("OK");
+  Serial.print("   IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void newConnectionCallback(uint32_t nodeId) 
-{
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
-    askAboutGroot();
-}
-
-void changedConnectionCallback() 
-{
-    Serial.printf("Changed connections\n");
-    askAboutGroot();
-}
-
-void nodeTimeAdjustedCallback(int32_t offset) 
-{
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
-}
+boolean isLightOn=false, prevSwitchState, isOtaUploading=false;
+String nodeId, msg, command_topic, state_topic;
+unsigned long prevMillisMqttReconnect=0, intervalMqttReconnect=5000;
+int mqttReconnectCounter=0, addr = 0;
 
 void setup() 
-{  
+{
   EEPROM.begin(512);
   isLightOn=EEPROM.read(addr);
-  digitalWrite(RELAY, isLightOn);
   pinMode(RELAY, OUTPUT);
-  pinMode(SWITCH, INPUT_PULLUP);
+  digitalWrite(RELAY, isLightOn);
   EEPROM.write(addr, false);
   EEPROM.commit(); 
+
+  pinMode(SWITCH, INPUT_PULLUP);
   
   Serial.begin(115200);
+  Serial.println("\r\nSetting up....");
+  nodeId=String(ESP.getChipId());
+  String hostName="LightSwitch-"+nodeId;
+  ArduinoOTA.setHostname(hostName.c_str());
+ 
+  setup_wifi();
 
-//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
-  mesh.setDebugMsgTypes( ERROR | STARTUP );
-  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
-  mesh.onReceive(&receivedCallback);
-  mesh.onNewConnection(&newConnectionCallback);
-  mesh.onChangedConnections(&changedConnectionCallback);
-  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+  Serial.print("Configuring OTA device...");
+  //TelnetServer.begin();
+  ArduinoOTA.onStart([]() {Serial.println("OTA starting...");isOtaUploading=true;});
+  ArduinoOTA.onEnd([]() {Serial.println("OTA update finished!");Serial.println("Rebooting...");});
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {Serial.printf("OTA in progress: %u%%\r\n", (progress / (total / 100)));}); 
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OK");
 
-  mesh.initOTAReceive("LightSwitch");
-  nodeId=String(mesh.getNodeId());
-  topic="Home/"+nodeId+"/state";
-  prevSwitchState=digitalRead(SWITCH);
-  prevMillisConnectionTest=millis();
+  Serial.println("Configuring MQTT server...");
+  Serial.printf("   Server IP: %s\r\n",mqtt_server); 
+  Serial.printf("   Username:  %s\r\n",mqtt_user);
+  Serial.println("   MQTT configured!");
+
+  Serial.println("Setup complete. Running loop...");
   
-  Serial.print("My state topic: ");Serial.println(topic);
-  Serial.print("My command topic: ");Serial.print("Home/");Serial.println(nodeId);
+  command_topic="Home/"+nodeId;
+  state_topic="Home/"+nodeId+"/state";
+  Serial.println("==================================================");
+  Serial.print("MQTT command topic: ");Serial.println(command_topic);
+  Serial.print("MQTT state topic: ");Serial.println(state_topic);
+  Serial.println("==================================================");
+  prevSwitchState=digitalRead(SWITCH);
+}
+
+void mqtt_reconnect() 
+{
+  if(!mqtt_client.connected() && millis()-prevMillisMqttReconnect >= intervalMqttReconnect) 
+    {
+      Serial.print("Attempting MQTT connection...");   
+      mqttReconnectCounter++;
+      if (mqtt_client.connect(nodeId.c_str(), mqtt_user, mqtt_password)) 
+      {
+        Serial.println("connected");
+        mqtt_client.subscribe(command_topic.c_str());
+        mqttReconnectCounter=0;
+      } 
+      else 
+      {
+        Serial.print("failed, rc=");
+        Serial.print(mqtt_client.state());
+        Serial.println(" try again in 5 seconds");
+        prevMillisMqttReconnect=millis();
+        if(mqttReconnectCounter>=3)
+        {
+          WiFi.disconnect();
+          Serial.println("Connection test failed. Restarting");
+          EEPROM.write(addr, isLightOn);
+          EEPROM.commit();
+          delay(100);
+          ESP.restart();
+        }
+      }
+    }
 }
 
 void loop() 
 {
- mesh.update();
- connectionTest();
+  do{ArduinoOTA.handle();}while(isOtaUploading==true);
+  mqtt_reconnect();
+  mqtt_client.loop();
+
+  if(digitalRead(SWITCH) != prevSwitchState)
+  {
+    isLightOn = !isLightOn;
+    digitalWrite(RELAY, isLightOn);
+    //if(isLightOn){msg="ON";}
+    //else{msg="OFF";}
+    msg = isLightOn ? "ON" : "OFF";
+    mqtt_client.publish(state_topic.c_str(), msg.c_str());
+    delay(100);
+    prevSwitchState=digitalRead(SWITCH);
+  }  
+}
+
+void mqttCallback(char* topic, uint8_t* payload, unsigned int length) 
+{
+  char* cleanPayload = (char*)malloc(length+1);
+  payload[length] = '\0';
+  memcpy(cleanPayload, payload, length+1);
+  String msg = String(cleanPayload);
+  free(cleanPayload);
+  Serial.println("recived "+msg);
+  if(msg=="ON")
+  {
+    isLightOn=true;
+    msg = "ON";
+    mqtt_client.publish(state_topic.c_str(), msg.c_str());
+    digitalWrite(RELAY, HIGH);
+  }
+  else if(msg=="OFF")
+  {
+    isLightOn=false;
+    msg = "OFF";
+    mqtt_client.publish(state_topic.c_str(), msg.c_str());
+    digitalWrite(RELAY, LOW);
+  }
+}
  
- if(digitalRead(SWITCH) != prevSwitchState)
- {
-  isLightOn = !isLightOn;
-  digitalWrite(RELAY, isLightOn);
-  sendMessageAndTopic(isLightOn ? "ON" : "OFF", topic);
-  prevSwitchState=digitalRead(SWITCH);
- }
-}
-
-void sendMessageAndTopic(String msgToSend, String topicToSend)
-{
-  if(mesh.isConnected(GrootId))
-  {
-    Serial.println("Sending: "+topicToSend+" "+msgToSend+" To: "+GrootId);
-    String packedMessage = topicToSend+","+msgToSend;
-    mesh.sendSingle(GrootId, packedMessage);
-  }
-  else
-  {
-    Serial.println("Message sending ERROR: Root not found");
-    isGrootConnected=false;
-    askAboutGroot();
-  }
-}
-
-void sendStartupMessage()
-{
-  if(mesh.isConnected(GrootId))
-  {
-    mesh.sendSingle(GrootId, isLightOn ? topic+",ON" : topic+",OFF");
-    isGrootConnected=true;
-  }
-  else
-  {
-    isGrootConnected=false;
-    askAboutGroot();
-  }
-}
-
-void askAboutGroot()
-{
-  if(isGrootConnected && mesh.isConnected(GrootId))
-  {
-    Serial.println("Groot already connected");
-  }
-  else
-  {
-    Serial.println("Groot asking");
-    isGrootConnected=false;
-    mesh.sendBroadcast(RootAskingMessage);
-  }
-}
-
-void connectionTest()
-{
-  if(millis()-prevMillisConnectionTest >= intervalConnectionTest)
-  {
-    Serial.println("Connection test start");
-    isTestPassed=false;
-    isConnectionTesting=true;
-    if(isGrootConnected && mesh.isConnected(GrootId))
-    {
-      mesh.sendSingle(GrootId, "ping");
-    }
-    else
-    {
-      askAboutGroot();
-      delay(500);
-      if(isGrootConnected && mesh.isConnected(GrootId))
-      {
-        mesh.sendSingle(GrootId, "ping");
-      }
-      else
-      {
-        Serial.println("Connection test failed. Restarting");
-        EEPROM.write(addr, isLightOn);
-        EEPROM.commit();
-        delay(1000);
-        ESP.restart();
-      }
-    }
-    prevMillisConnectionTestWait=millis();
-    prevMillisConnectionTest=millis();
-  }
-  if(millis()-prevMillisConnectionTestWait >= intervalConnectionTestWait && isTestPassed==false && isConnectionTesting==true)
-  {
-    Serial.println("Connection test failed. Restarting");
-    EEPROM.write(addr, isLightOn);
-    EEPROM.commit(); 
-    delay(1000);
-    ESP.restart();
-  }
-  else if(millis()-prevMillisConnectionTestWait >= intervalConnectionTestWait && isTestPassed==true && isConnectionTesting==true)
-  {
-    Serial.println("Ping was ponged by Groot. Connection test passed");
-    isConnectionTesting=false;
-  }
-}
